f4e5196ad948e066d476463c3bdede95bbc2ec22
jsiyoon <37797075+jsiyoon@users.noreply.github.com>
2022-04-17 21:19:40 +0900

    Lv1. 3진법 뒤집기

--- src/J_68935.java
+++ src/J_68935.java
@@ -1,4 +1,28 @@
-
+import java.io.*;
+import java.util.*;
 public class J_68935 {
-
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num = Integer.parseInt(br.readLine());
+		int answer = 0;
+		
+		Stack<Integer> st = new Stack<>();
+		while(num > 0) {
+			st.add(num % 3);
+			num /= 3;
+		}
+		
+		int count = 1;
+		while(!st.isEmpty()) {
+			answer += st.pop() * count;
+			count *= 3;
+		}
+		
+		bw.write(String.valueOf(answer));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
 }
