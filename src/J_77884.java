87ec7da3c99e5796799b888a0ab2a246dda95715
jsiyoon <37797075+jsiyoon@users.noreply.github.com>
2022-04-18 10:57:19 +0900

    약수의 개수와 덧셈

---
+++ src/J_77884.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class J_77884 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int left = 13;
+		int right = 17;
+		
+		int answer = 0;
+        int count = 0;
+        for(int i = left;  i <= right; i++){
+            for(int j = 1; j <= i; j++){
+                if(i % j == 0) count ++;
+            }
+            if(count % 2 == 0) answer += i;
+            else answer -= i;
+            count = 0;
+        }
+		
+        bw.write(String.valueOf(answer));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
